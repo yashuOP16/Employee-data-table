fef8f4a2ac36ea43d195ee2654b2be6232e95b33
yashuOP16 <yash.s.p86@gmail.com>
2024-07-11 15:09:19 +0530

    Empolyee-data-table

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+// src/index.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
